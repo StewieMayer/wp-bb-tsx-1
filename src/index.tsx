2c347ff94ac3b0ac044262d58c38af266ec777e9
stewie <stewiemayer@hotmail.com>
2025-08-01 15:49:38 -0600

    feat: se incluye el mensaje inicial

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom/client';
 
 const rootElement = document.getElementById('root')
 const root = ReactDOM.createRoot(rootElement!);
-root.render(<h1>Hola mundo</h1>);
+root.render(<h1>Bienvenidos al Himalaya!</h1>);
