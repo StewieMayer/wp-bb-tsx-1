15fb338cb750a02d7af2835dfa6587ac38d5b3ef
stewie <stewiemayer@hotmail.com>
2025-08-05 14:35:00 -0600

    feat: se crea el componente app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './styles/index.css';
+
+const App = () =>{
+    return (
+        <></>
+    )
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement!);
-root.render(<h1>Bienvenidos al Himalaya!</h1>);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
