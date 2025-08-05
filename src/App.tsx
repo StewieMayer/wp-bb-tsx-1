6dd99c84297607006b39e55174b23692c2e48793
stewie <stewiemayer@hotmail.com>
2025-08-05 15:14:09 -0600

    feat: se integra Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import './styles/index.css';
+import Layout from "./components/Layout";
+import "./styles/index.css";
 
-const App = () =>{
-    return (
-        <></>
-    )
-}
+const App = () => {
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold underline text-center">
+        Hola, Mundo! ¡Bienvenido a mi aplicación React con TypeScript, Babel y
+        Webpack!
+      </h1>
+    </Layout>
+  );
+};
 
 export default App;
