7e82025ba9da2c72c0e42f256359b86bb08350b0
stewie <stewiemayer@hotmail.com>
2025-08-05 15:13:43 -0600

    feat: se crea Layout y se integran Header & Footer

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import type { PropsWithChildren } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <main className="flex-1 container mx-auto px-4 py-8 max-w-7xl">
+        {children}   
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
